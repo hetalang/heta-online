9854b43f5f7f148d98d573a2fe66fb3e36d7ade1
Evgeny Metelkin <metelkin@insysbio.com>
2023-08-26 11:02:55 +0300

    update view

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-import 'w3css/w3.css'
-import 'font-awesome/css/font-awesome.min.css'
+// Add styles and fonts
+import 'w3-css/w3.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+// Auxilary
+import $ from 'jquery';
+import * as monaco from 'monaco-editor';
+
+$(window).on('resize', updateWindowHeight);
+
+// Events
+
+// document ready
+$(() => {
+    updateWindowHeight();
+/*
+    monaco.editor.create(document.getElementById('container'), {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+        language: 'javascript',
+        automaticLayout: true
+    });
+*/
+});
+
+function updateWindowHeight(){
+    let h = document.documentElement.clientHeight - $('#topDiv').outerHeight();
+    $('#mainDiv').height(h + 'px');
+    //console.log($(document.documentElement).height());
+}
+
+// Monaco
+/*
+monaco.editor.create(document.getElementById('container'), {
+	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+	language: 'javascript'
+});
+*/
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -24,6 +25,7 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'src/index.html',
         }),
+        new MonacoWebpackPlugin(),
 
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
