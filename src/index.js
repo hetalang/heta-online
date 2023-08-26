174426e7860abbb977e9dc9320cdb1d7c18fa25e
Evgeny Metelkin <metelkin@insysbio.com>
2023-08-26 14:24:48 +0300

    monaco works

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import 'font-awesome/css/font-awesome.min.css';
 
 // Auxilary
 import $ from 'jquery';
-import * as monaco from 'monaco-editor';
+//import * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 $(window).on('resize', updateWindowHeight);
 
@@ -13,25 +14,38 @@ $(window).on('resize', updateWindowHeight);
 // document ready
 $(() => {
     updateWindowHeight();
-/*
-    monaco.editor.create(document.getElementById('container'), {
+    monaco.editor.create(document.getElementById('indexHetaContainer'), {
         value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-        language: 'javascript',
-        automaticLayout: true
+        language: 'javascript'
     });
-*/
 });
 
 function updateWindowHeight(){
     let h = document.documentElement.clientHeight - $('#topDiv').outerHeight();
     $('#mainDiv').height(h + 'px');
-    //console.log($(document.documentElement).height());
 }
 
-// Monaco
 /*
-monaco.editor.create(document.getElementById('container'), {
-	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-	language: 'javascript'
-});
+//  This modul imports different types of modules
+
+
+#setNS {space: one, type: abstract};
+
+include json.json type json
+
+include heta.heta type heta
+
+include yaml.yml type yaml
+
+include xlsx.xlsx type xlsx with {
+  options: { sheetNum: [0, 1], omitRows: 3 }
+}
+
+include page.md type md with {
+  options: {pageId: xxx},
+  id: yyy,
+  title: hhh
+};
+
+#export {format: YAML, filepath: yaml};
 */
