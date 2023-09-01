4eb1fff303d76cb5459ca9a5c9f1f8a5c4597dce
Evgeny Metelkin <metelkin@insysbio.com>
2023-09-01 16:44:05 +0300

    add modules types

--- src/heta-editor.js
+++ src/heta-editor.js
@@ -3,9 +3,21 @@ import PLATFORM_JSON_TEMPLATE from './heta-templates/platform.json.template';
 import INDEX_HETA_TEMPLATE from './heta-templates/index.heta.template';
 import QSP_UNITS_HETA_TEMPLATE from './heta-templates/qsp-units.heta.template';
 import DEFAULT_HETA_TEMPLATE from './heta-templates/default.heta.template';
+import DEFAULT_JSON_TEMPLATE from './heta-templates/default.json.template';
+import DEFAULT_CSV_TEMPLATE from './heta-templates/default.csv.template';
+import DEFAULT_YAML_TEMPLATE from './heta-templates/default.yaml.template';
+import DEFAULT_XML_TEMPLATE from './heta-templates/default.xml.template';
 
 import $ from 'jquery';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
+const FORMATS = {
+    json: {fileType: 'json', template: DEFAULT_JSON_TEMPLATE},
+    heta: {fileType: 'heta', template: DEFAULT_HETA_TEMPLATE},
+    csv: {fileType: 'csv', template: DEFAULT_CSV_TEMPLATE},
+    yaml: {fileType: 'yaml', template: DEFAULT_YAML_TEMPLATE},
+    xml: {fileType: 'xml', template: DEFAULT_XML_TEMPLATE},
+}
 
 // class storing HetaEditors
 class HetaEditorsCollection {
@@ -21,21 +33,24 @@ class HetaEditorsCollection {
         this.hetaEditorsStorage = new Map();
         // set events
         let counter = 0;
-        $('#addModuleBtn').on('click', () => {
-            this.addEditor(`index${counter++}.heta`);
+        $('#newButton').on('change', (evt) => {
+            let format = FORMATS[evt.target.value];
+            if (format==undefined) throw new Error('Unknown Heta module format.');
+            this.addEditor(`module${counter++}.${format.fileType}`, format.template, format.fileType);
+            evt.target.value = '';
         })
     }
     static createWithDefaults() {
         let hpc = new HetaEditorsCollection();
         hpc.addEditor('platform.json', PLATFORM_JSON_TEMPLATE, 'json', false, true);
         hpc.addEditor('qsp-units.heta', QSP_UNITS_HETA_TEMPLATE, 'json');
-        hpc.addEditor('index.heta', INDEX_HETA_TEMPLATE, 'json', false, false);
+        hpc.addEditor('index.heta', INDEX_HETA_TEMPLATE, 'heta', false, false);
 
         return hpc;
     }
     addEditor(id, initialCode=DEFAULT_HETA_TEMPLATE, type='HetaCode', toDelete=true, rightSide=false) {
         // add to storage
-        let hp = new HetaEditor(id, initialCode, toDelete, rightSide);
+        let hp = new HetaEditor(id, initialCode, type, toDelete, rightSide);
         hp._parent = this;
         this.hetaEditorsStorage.set(id, hp);
         this.showEditor(id);
@@ -47,7 +62,6 @@ class HetaEditorsCollection {
     showEditor(id) {
         this.hideEditors();
         let panel = this.hetaEditorsStorage.get(id);
-        console.log(panel)
 
         $(panel.navigationButton).addClass('w3-bottombar w3-border-green');
         $(panel.editorContainer).css('display', 'block');
@@ -63,24 +77,24 @@ class HetaEditorsCollection {
 
 // class storing editor and visualization
 class HetaEditor {
-    constructor(id, initialCode, toDelete=true, rightSide=false, readOnly=false) {
+    constructor(id, initialCode, type='json', toDelete=true, rightSide=false, readOnly=false) {
         this.id = id;
 
         // create div element
         this.editorContainer = $(`<div class="hetaModuleContainer" style="height:100%;"></div>`)
             .appendTo($('#codeContainerLeft'))[0];
-        this.navigationButton = $(`<a href="#" class="hetaModuleBtn w3-bar-item w3-button"><span class="hetaModuleName">${id}</span></a>`)
+        this.navigationButton = $(`<a href="#" class="hetaModuleBtn w3-button w3-small"><span class="hetaModuleName">${id}</span></a>`)
             .appendTo($('#codeNaviLeft'))[0];
         if (rightSide) {
             $(this.navigationButton).addClass('w3-right');
         }
 
         // add events to module
-        $(this.navigationButton).find('.hetaModuleName').on('click', () => { // 
+        $(this.navigationButton).find('.hetaModuleName').on('click', () => {
             this._parent.showEditor(id);
         });
         if (toDelete) {
-            let closeBtn = $('<span class="hetaModuleCloseBtn">&nbsp; &times;</span>')
+            $('<span class="hetaModuleCloseBtn">&nbsp; &times;</span>')
                 .appendTo(this.navigationButton)
                 .on('click', () => {
                     this._parent.deleteEditor(id);
@@ -88,9 +102,9 @@ class HetaEditor {
         }
 
         // add editor
-        this.editor = monaco.editor.create(this.editorContainer, {
+        this.monacoEditor = monaco.editor.create(this.editorContainer, {
             value: initialCode,
-            language: 'json',
+            language: type ,
             readOnly: readOnly
         });
     }
