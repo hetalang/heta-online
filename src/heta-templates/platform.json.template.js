f6fead7305dc2184e0bb978362edc8d473e2d323
Evgeny Metelkin <metelkin@insysbio.com>
2023-10-28 18:07:56 +0300

    update template

--- src/heta-templates/platform.json.template.js
+++ src/heta-templates/platform.json.template.js
@@ -1,10 +1,19 @@
 export default `{
-  "builderVersion": "^0.6.16",
+  "$schema": "https://github.com/hetalang/heta-compiler#",
   "id": "template",
   "notes": "platform notes",
   "version": "v0.1.0",
+  "keywords": [],
   "license": "UNLICENSED",
-  "options": {},
+  "contributors": [],
+  "builderVersion": "^0.6.19",
+  "options": {
+    "debug": false,
+    "logs": "output.log",
+    "logLevel": "info",
+    "skipExport": false,
+    "unitsCheck": false
+  },
   "importModule": {
     "type": "heta",
     "source": "index.heta"
